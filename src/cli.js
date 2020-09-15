04d6f34fb4e4e03c6e7a84c39ee1231e52dd9d4a
Roberto Sousa <robertosousa186@gmail.com>
2020-09-15 00:59:52 -0300

    Adicionando arquivos a pasta src

--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,33 @@
 const program = require('commander');
 
-const { version } = require('../package.json')
+const {
+    version
+} = require('../package.json');
+
+const getForecast = require('./main');
+const {
+    saveApiToken
+} = require('./utils/apiToken');
+
+export function init(args) {
+    program
+        .version(version, '-v, --version', 'Mostra a versão da ferramenta')
+        .option('-t --token [token]', 'Advisor ClimaTempo API token')
+        .arguments('<cityName...>')
+        .description('Mostra o clima de uma cidade em tempo real')
+        .action(async (cityName) => {
+            if (program.token) {
+                await saveApiToken(program.token)
+            }
+
+            getForecast(cityName.join(' '))
+        })
+        .on('--help', () => {
+            console.log()
+            console.log('Exemplos:')
+            console.log('$ clima porto alegre')
+            console.log('$ clima são paulo')
+        })
+
+    program.parse(args)
+}
